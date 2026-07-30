d7cd06772ac1b011ae8ec83e065a33c50e6803b5
NarriTrail <battanaresh456@gmail.com>
2026-07-30 22:20:10 +0530

    bash file and workflow updated

---
+++ .github/workflows/bashfile.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+echo "this is my bash file script" > app.txt
+ls -a
+touch hello.js
+ls -ltr hello.js
+cat app.txt
+uname
+date
+pwd
+chmod +x hello.js
+ls -ltr hello.js
+mkdir firstFolder
+cd firstFolder
+echo "first file content" > firstFile.txt
