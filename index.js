1d7dab3bd7ecd58bd186f4cf8ae72947e181cf1b
NarriTrail <battanaresh456@gmail.com>
2026-08-04 22:45:42 +0530

    feat: my first nodejs workflow

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-console.log("hello Narri")
+const express = require('express')
+const app= express()
+
+app.get('/',(req,res)=>{
+    res.send({
+        name:"hello world hii"
+    })
+})
+app.listen(3001,()=>{
+   console.log("server running at"+ " "+ 3001)
+})
