c0ec3df32111d8bdde7ce9b29bb5235840fc31d3
NarriTrail <battanaresh456@gmail.com>
2026-08-05 23:17:48 +0530

    feat: added lint workflow

---
+++ eslint.config.js
@@ -0,0 +1,13 @@
+const js = require("@eslint/js");
+
+module.exports = [
+  js.configs.recommended,
+  {
+    rules: {
+      // formatting rule so incorrect spacing fails the build too
+      "indent": ["error", 2],
+      "quotes": ["error", "double"],
+      "semi": ["error", "always"]
+    }
+  }
+];
